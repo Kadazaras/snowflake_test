dd4d5b4dc5be6b2382fbff41a4e1efaca1e2c05d
alberto <amgo8>
2022-05-09 00:43:10 +0200

    new commit

--- create_csv.py
+++ create_csv.py
@@ -1,13 +1,19 @@
 import numpy as np
 import os
 
-filenames = ['file1.csv','file2.csv','file3.csv','file4.csv']
+filenames = ['file{}.csv'.format(i) for i in range(10)]
 
+if os.path.isdir(os.path.join(os.getcwd(),'data')):
+    pass
+else:
+    os.mkdir(os.path.join(os.getcwd(),'data'))
+
+fileDir = os.path.join(os.getcwd(),'data')
 
 for i in filenames:
-    with open(os.path.join(os.getcwd(),i),'w') as f:
+    with open(os.path.join(fileDir,i),'w') as f:
         f.write('a,b,c,d,e,f,g\n')
-        for i in range(1000000):
+        for i in range(4000000):
             string_complete = ','.join([str(i) for i in np.random.rand(7)])
             string_complete = string_complete + '\n'
             f.write(string_complete)
--- snowflake_testing.py
+++ snowflake_testing.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 import snowflake.connector
 import os
+import threading
+import random
+import time
 
 # Gets the version
 ctx = snowflake.connector.connect(
     user='albertomartinezgarcia',
     password='25108-Snowflake8',
     account='pzb06830',
-    region='us-east-1'
+    region='us-east-1',
+    client_session_keep_alive=True
     )
 cs = ctx.cursor()
 if False:
@@ -25,6 +29,7 @@ if False:
         cs.execute("""create or replace file format my_csv_format
                       type = csv
                       field_delimiter = ','
+                      SKIP_HEADER = 1
                       null_if = ('NULL', 'null')
                       empty_field_as_null = true
                       compression = gzip;""")
@@ -42,7 +47,7 @@ if False:
     finally:
         cs.close()
 
-if False:
+if True:
     try:
         cs.execute("USE DATABASE testdb")
         cs.execute("USE SCHEMA testschema")
@@ -75,17 +80,85 @@ if False:
     finally:
         cs.close()
 
-if True:
+if False:
     try:
         cs.execute("USE DATABASE testdb")
         cs.execute("USE SCHEMA testschema")
         cs.execute("alter warehouse compute_wh resume if suspended;")
-        cs.execute("copy into testtable from @teststage;")
+        cs.execute("copy into testtable from @teststage FILE_FORMAT = (FORMAT_NAME = 'my_csv_format');")
     finally:
         cs.close()
 
-        
-        
-       
+if False:
+    try:
+        cs.execute("USE DATABASE testdb")
+        cs.execute("USE SCHEMA testschema")
+        cs.execute("alter warehouse compute_wh resume if suspended;")
+        cs.execute("remove @teststage pattern='.*.csv.gz';")
+        for (name,result) in cs:
+            print('{},{}'.format(name,result))
+    finally:
+        cs.close()
 
+class ConcurrentQuery (threading.Thread):
+    def __init__(self,name,file):
+        threading.Thread.__init__(self, name=name)
+        self.file = file
+        self.ctx = snowflake.connector.connect(
+            user='albertomartinezgarcia',
+            password='25108-Snowflake8',
+            account='pzb06830',
+            region='us-east-1',
+            client_session_keep_alive=True
+        )
+        self.cs = self.ctx.cursor()
+    def run(self):
+        query = "copy into testtable from @{} FILE_FORMAT = (FORMAT_NAME = 'my_csv_format');".format(self.file)
+        print('Copying {}'.format(self.file))
+        self.cs.execute(query)
+
+
+
+
+
+if False:
+    cs.execute("USE DATABASE testdb")
+    cs.execute("USE SCHEMA testschema")
+    cs.execute("alter warehouse compute_wh resume if suspended;")
+    cs.execute("LIST @teststage")
+    filelist = []
+    for (name,size,md5,last_modified) in cs:
+        filelist.append(name)
+
+
+    clusters = 4
+    print_lock = threading.Lock()
+    threads = []
+
+    for i in range(clusters):
+        thread = ConcurrentQuery(name=str(i),file=filelist.pop(0))
+        threads.append(thread)
+
+    for thread in threads:
+        thread.start()
+
+    while len(filelist)>0:
+        for thread in threads:
+            if thread.is_alive():
+                pass
+            else:
+                with print_lock:
+                    print('Thread {} finished: restart new thread'.format(thread.name))
+                thread.__init__(name=thread.name,file=filelist.pop(0))
+                thread.start()
+    while True:
+        checklist = []
+        for thread in threads:
+            if thread.is_alive():
+                checklist.append(False)
+            else:
+                checklist.append(True)
+        if all(checklist):
+            break
+        time.sleep(1)
 ctx.close()
